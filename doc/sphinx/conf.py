e5288973e1b1874dea7f9a57cf77a795172fb763
tknopp <tobias@knoppweb.de>
2013-03-27 16:43:01 +0100

    Update conf.py

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -44,9 +44,9 @@ copyright = '2013, Tobias Knopp'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
-release = '0.0'
+release = '0.1alpha0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
