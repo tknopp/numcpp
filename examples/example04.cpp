2df1e0c8580f0d1d56fce2dd556e4fe63e419b43
tknopp <tobias@knoppweb.de>
2013-04-11 18:16:07 +0200

    minor

--- examples/example04.cpp
+++ examples/example04.cpp
@@ -7,38 +7,76 @@ int main()
                   4,5,6,
                   7,8,9}, 3, 3);
 
-  auto y = x;                     // create a view of x
+  auto y = x;                                    // create a view of x
+ 
+  auto z = copy(x);                              // create a deep copy of x
 
-  auto z = copy(x);               // create a deep copy of x
+  auto x1 = x(2,2);                              // access element at index 2,2  
+  print(x1);                                     // x1 = 9
 
-  x(slice(0,end,2), full) = 1;    // set first and last row to one
-  print(x);                       // [1 1 1; 4 5 6; 1 1 1]
+  auto x2 = x( slice(0,2), slice(0,2) );         // select block within x
+  print(x2);                                     // x2 = [1 2; 4 5]
 
-  x(1, full) = z(0, full);        // copy first row of z into the second row of x
-  print(x);                       // [1 1 1; 1 2 3; 1 1 1]
+  auto x3 = x( slice(0,end,2), slice(0,end,2) ); // select strided block within x
+  print(x3);                                     // x3 = [1 3; 7 8]
+
+  auto x4 = x( full, 0 );                        // select first row of x
+  print(x4);                                     // x4 = [1 2 3]
+
+  x(slice(0,end,2), full) = 1;                   // set first and last row to one
+  print(x);                                      // [1 1 1; 4 5 6; 1 1 1]
+
+  x(1, full) = z(0, full);                       // copy first row of z into the second row of x
+  print(x);                                      // [1 1 1; 1 2 3; 1 1 1]
+
+  auto u = eval(range(0,10));                    // create array u = [0 ... 9]
+  std::vector<int> indices = {0, 1, 7, 5};       // create index vector
+  auto u1 = u(indices);                          // non-regular indexing of u
+  print(u1);                                     // u1 = [0 1 6 4]
 }
 
 /*!
 @page example04 Array Slicing
-This is a simple example showing the slicing capabilities of NumCpp.
+This is a simple example showing the indexing an slicing capabilities of NumCpp.
 
 @code
+#include <numcpp.h>
+using namespace numcpp;
+
 int main()
 {
   auto x = array({1,2,3,
                   4,5,6,
                   7,8,9}, 3, 3);
 
-  auto y = x;                     // create a view of x
+  auto y = x;                                    // create a view of x
+ 
+  auto z = copy(x);                              // create a deep copy of x
 
-  auto z = copy(x);               // create a deep copy of x
+  auto x1 = x(2,2);                              // access element at index 2,2  
+  print(x1);                                     // x1 = 9
 
-  x(slice(0,end,2), full) = 1;    // set first and last row to one
-  print(x);                       // [1 1 1; 4 5 6; 1 1 1]
+  auto x2 = x( slice(0,2), slice(0,2) );         // select block within x
+  print(x2);                                     // x2 = [1 2; 4 5]
 
-  x(1, full) = z(0, full);        // copy first row of z into the second row of x
-  print(x);                       // [1 1 1; 1 2 3; 1 1 1]
+  auto x3 = x( slice(0,end,2), slice(0,end,2) ); // select strided block within x
+  print(x3);                                     // x3 = [1 3; 7 8]
+
+  auto x4 = x( full, 0 );                        // select first row of x
+  print(x4);                                     // x4 = [1 2 3]
+
+  x(slice(0,end,2), full) = 1;                   // set first and last row to one
+  print(x);                                      // [1 1 1; 4 5 6; 1 1 1]
+
+  x(1, full) = z(0, full);                       // copy first row of z into the second row of x
+  print(x);                                      // [1 1 1; 1 2 3; 1 1 1]
+
+  auto u = eval(range(0,10));                    // create array u = [0 ... 9]
+  std::vector<int> indices = {0, 1, 7, 5};       // create index vector
+  auto u1 = u(indices);                          // non-regular indexing of u
+  print(u1);                                     // u1 = [0 1 6 4]
 }
+
 @endcode
 */
 
