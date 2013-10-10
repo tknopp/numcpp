2115017ae7af7fa1e08f7e8f952799ed151a164a
Tobias Knopp <tobias@knoppweb.de>
2013-10-11 01:06:44 +0200

    n

--- numcpp/numcpp/core/abstractexpression.h
+++ numcpp/numcpp/core/abstractexpression.h
@@ -46,6 +46,37 @@ protected:
   Derived const & getSelf() const { return *static_cast<Derived const *>(this); }
 };
 
+template<class T, class Derived>
+class AbstractSparseExpression
+{
+public:
+  typedef T value_type;
+
+  size_t size() const
+  {
+    return getSelf().size();
+  }
+
+  const std::vector<size_t>& shape() const
+  {
+    return getSelf().shape();
+  }
+
+  T& operator[](size_t index)
+  {
+    return getSelf()[index];
+  }
+
+  T operator[](size_t index) const
+  {
+    return getSelf()[index];
+  }
+
+protected:
+  Derived & getSelf() { return *static_cast<Derived *>(this); }
+  Derived const & getSelf() const { return *static_cast<Derived const *>(this); }
+};
+
 
 /*! @} */
 
